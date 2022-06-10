349e6f69cfe6146a2e83d53df55359df04a62bf7
matataforex <matataforex@gmail.com>
2022-06-10 20:04:30 +0300

    last commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <div class="row ">
             <div class="col-md-8">
                 <div class="input-group mb-3">
-                        <input type="file" class="form-control" name="cargo_file" accept=".xlsx,.xls,.csv" placeholder="select your excel file" aria-label="Recipient's username"  required>
+                        <input type="file" class="form-control" name="cargo_file" accept=".xlsx,.xls,.csv" placeholder="select your excel file" aria-label="select your excel file"  required>
                     </div>  
             </div>
             <div class="col-md-4">
